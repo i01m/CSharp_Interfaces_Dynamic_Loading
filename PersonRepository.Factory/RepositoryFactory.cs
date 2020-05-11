0155403292f1397dfb93603b9c708db99062bb7d
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-11 14:26:30 -0500

    first commit

---
+++ PersonRepository.Factory/RepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using PersonRepository.Interface;
+using System;
+using System.Configuration;
+
+namespace PersonRepository.Factory
+{
+    public static class RepositoryFactory
+    {
+        public static IPersonRepository GetRepository()
+        {
+            string repositoryName = ConfigurationManager.AppSettings["RepositoryType"];
+            Type repositoryType = Type.GetType(repositoryName);
+            object repository = Activator.CreateInstance(repositoryType);
+            IPersonRepository personRepository = repository as IPersonRepository;
+
+            return personRepository;
+        }
+    }
+}
